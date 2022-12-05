97ef8cd30b84dfd207bea4407fbab481cbc815c7
israeljrx <franciscoisrael@outlook.com.br>
2022-12-05 19:41:04 -0400

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+const headerFaqs = document.querySelectorAll('article header');
+const paragraphAnswer = document.querySelectorAll('article p');
+const titleFaqs = document.querySelectorAll('article header h1');
+const arrowTitle = document.querySelectorAll('article header img');
+
+function toggleAnswer(index) {
+  titleFaqs[index].classList.toggle('active-title');
+  paragraphAnswer[index].classList.toggle('close');
+  paragraphAnswer[index].classList.toggle('open');
+  arrowTitle[index].classList.toggle('active-img');
+}
+
+headerFaqs.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    toggleAnswer(index);
+  });
+});
